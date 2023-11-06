3af2eb2a38f3b77a3427085b85e827db5a014395
Abhishek Patel <abhipatel9607@gmail.com>
2023-11-06 15:01:55 +0530

    Control toggle with Keyboard Features Added

--- src/script.js
+++ src/script.js
@@ -1,19 +1,28 @@
 const toggleCheckbox = document.getElementById("toggleCheckbox");
-const price_basic = document.querySelector(".price_basic")
-const price_professional = document.querySelector(".price_professional")
-const price_master = document.querySelector(".price_master")
+const price_basic = document.querySelector(".price_basic");
+const price_professional = document.querySelector(".price_professional");
+const price_master = document.querySelector(".price_master");
 
-
-toggleCheckbox.addEventListener("change", function () {
+// Function to toggle prices
+function togglePrices() {
     if (toggleCheckbox.checked) {
         price_basic.innerText = "199.99";
-        price_professional.innerText = "249.99"
-        price_master.innerText = "399.99"
-
+        price_professional.innerText = "249.99";
+        price_master.innerText = "399.99";
     } else {
         price_basic.innerText = "19.99";
-        price_professional.innerText = "24.99"
-        price_master.innerText = "39.99"
+        price_professional.innerText = "24.99";
+        price_master.innerText = "39.99";
+    }
+}
 
+// Toggle prices when the checkbox is checked or unchecked
+toggleCheckbox.addEventListener("change", togglePrices);
+
+// Manage keyboard interaction
+toggleCheckbox.addEventListener("keydown", function (event) {
+    if (event.key === 'Enter') {
+        toggleCheckbox.click(); // Simulate a click when Enter is pressed
+        event.preventDefault(); // Prevent the default Enter behavior (form submission)
     }
 });
